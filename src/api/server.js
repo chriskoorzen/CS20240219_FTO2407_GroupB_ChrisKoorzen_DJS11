42011fda2e9eac1bf452e81e20516e0e9b548b44
Chris Koorzen <fckoorzen@gmail.com>
2024-11-23 22:33:27 +0200

    Add hardcoded genre info

--- src/api/server.js
+++ src/api/server.js
@@ -36,3 +36,15 @@ export async function getShowInfo(showID){
         }
     );
 };
+
+export const showGenres = {
+    1: "Personal Growth",
+    2: "Investigative Journalism",
+    3: "History",
+    4: "Comedy",
+    5: "Entertainment",
+    6: "Business",
+    7: "Fiction",
+    8: "News",
+    9: "Kids and Family",
+};
