0dce33377b90a7a8c94586d24b68e155e3ec4392
Chris Koorzen <fckoorzen@gmail.com>
2024-11-23 22:32:07 +0200

    Create main layout

---
+++ src/page/layouts.jsx
@@ -0,0 +1,27 @@
+import { Outlet, useOutletContext, NavLink } from "react-router-dom";
+
+
+import { SiteHeader } from "../component/header";
+import { AudioPlayer } from "../component/audioplayer";
+import { DashBoard } from "../component/user";
+
+
+export function MainLayout(){
+
+    return (
+        <div className="size-full flex flex-col justify-between bg-gray-900">
+            <SiteHeader />
+
+            <div className="grow grid grid-cols-[360px_minmax(900px,_1fr)] gap-3">    
+                <div className="bg-gray-800 rounded-lg my-3 ml-3">
+                    <DashBoard />
+                </div>    
+                <div className="rounded-lg bg-gray-800 my-3 mr-3 p-4">
+                    <Outlet />
+                </div>
+            </div>
+
+            <AudioPlayer />
+        </div>
+    );
+};
