bdd09bda26a07aa8681c6d00334a5efeb2823800
Chris Koorzen <fckoorzen@gmail.com>
2024-11-23 22:32:54 +0200

    Update App routing layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { createHashRouter, createRoutesFromElements, Route } from "react-router-dom";
 
+import { MainLayout } from "./page/layouts";
+import { LandingPage } from "./page/hero";
+
+import { getPreviews } from "./api/server";
+
 
 export const App = createHashRouter(
   createRoutesFromElements(
-      <Route path="/" element={<h1>Podcast App</h1>} />
+      <Route path="/" element={<MainLayout />} id="root" loader={async()=>{ return {previews:  getPreviews()}; }}>
+        <Route index element={<LandingPage />} />
+      </Route>
   )
 );
