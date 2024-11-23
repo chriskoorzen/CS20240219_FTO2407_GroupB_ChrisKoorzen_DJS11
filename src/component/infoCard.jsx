7a00d73fcf22a78525f304139ac8e8ba137ba4e6
Chris Koorzen <fckoorzen@gmail.com>
2024-11-23 22:07:52 +0200

    Update showPreview component layout + styling

--- src/component/infoCard.jsx
+++ src/component/infoCard.jsx
@@ -1,26 +1,36 @@
 import { showGenres } from "../api/server";
 
+import { sanitizeHtmlLiterals } from "../utils/strings";
+
 
 export function FullShowPreview({ show }){
 
-    console.log("FullShowPreview", show)
+    // console.log("FullShowPreview", show)
 
     if (show === null){
         return <h1>Placeholder</h1>
     };
 
     return (
-        <div className="rounded-lg w-[600px] bg-green-900 p-3">
-            <img src={show.image} className="size-96"/>
-            <h6>{show.title}</h6>
-            <p>{show.seasons > 1 ? `${show.seasons} Seasons` : `${show.seasons} Season`}</p>
-            <p>{show.description}</p>
-            <p>{new Date(show.updated).toLocaleDateString(undefined, {year: "numeric",month: "short"})}</p>
-            {
-                show.genres.map(genreID => {
-                    return <p key={genreID}>{showGenres[genreID]}</p>
-                })
-            }
+        <div 
+            className="rounded-lg bg-gray-900 p-3 flex gap-5 text-white cursor-pointer"
+        >
+            <img src={show.image} className="size-96 rounded-lg"/>
+            <div>
+                <h6 className="text-2xl font-bold mb-4">{sanitizeHtmlLiterals(show.title)}</h6>
+                <div className="flex justify-between">
+                    <p>{show.seasons > 1 ? `${show.seasons} Seasons` : `${show.seasons} Season`}</p>
+                    <p>{new Date(show.updated).toLocaleDateString(undefined, {year: "numeric",month: "short"})}</p>
+                </div>
+                <div className="my-3 flex gap-5">
+                    {
+                        show.genres.map(genreID => {
+                            return <p key={genreID} className="bg-purple-500 rounded-full p-2">{showGenres[genreID]}</p>
+                        })
+                    }
+                </div>
+                <p className="line-clamp-6">{show.description}</p>
+            </div>
         </div>
     );
 };
