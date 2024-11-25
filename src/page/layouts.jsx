bf14720982e83fef9879ac3cd81b480e9b3b52fa
Chris Koorzen <fckoorzen@gmail.com>
2024-11-25 22:48:25 +0200

    Styling: Fix main outlet size

--- src/page/layouts.jsx
+++ src/page/layouts.jsx
@@ -1,7 +1,7 @@
 import { Outlet, useOutletContext, NavLink } from "react-router-dom";
 
 
-import { SiteHeader } from "../component/header";
+import { SiteHeader } from "../component/site";
 import { AudioPlayer } from "../component/audioplayer";
 import { DashBoard } from "../component/user";
 
@@ -15,9 +15,11 @@ export function MainLayout(){
             <div className="grow grid grid-cols-[360px_minmax(900px,_1fr)] gap-3">    
                 <div className="bg-gray-800 rounded-lg my-3 ml-3">
                     <DashBoard />
-                </div>    
-                <div className="rounded-lg bg-gray-800 my-3 mr-3 p-4">
-                    <Outlet />
+                </div>
+                <div className="grow relative">
+                    <div className="absolute inset-0 overflow-y-auto rounded-lg bg-gray-800 my-3 mr-3 p-4">
+                        <Outlet />
+                    </div>
                 </div>
             </div>
 
