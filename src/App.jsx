6a68477f1b6db49e45bacbd45d82a483bcf31a78
Chris Koorzen <fckoorzen@gmail.com>
2024-11-25 22:53:56 +0200

    Replace HashRouter with BrowserRouter, update app routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { createHashRouter, createRoutesFromElements, Route } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 
 import { MainLayout } from "./page/layouts";
 import { LandingPage } from "./page/hero";
+import { FullShowPage } from "./page/shows";
+import { BrowsePage } from "./page/browse";
 
-import { getPreviews } from "./api/server";
+import { loadInitialData } from "./api/server";
 
 
-export const App = createHashRouter(
-  createRoutesFromElements(
-      <Route path="/" element={<MainLayout />} id="root" loader={async()=>{ return {previews:  getPreviews()}; }}>
-        <Route index element={<LandingPage />} />
-      </Route>
-  )
+export const App = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<MainLayout />} id="root" loader={loadInitialData}>
+            <Route index element={<LandingPage />} />
+            <Route path="browse/" element={<BrowsePage />} />
+            <Route path="show/:showid/" element={<FullShowPage />}>
+
+            </Route>
+        </Route>
+    )
 );
