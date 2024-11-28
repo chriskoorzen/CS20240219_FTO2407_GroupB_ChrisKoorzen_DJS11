6c8e5dd9d86c0cfefff3e6fdba8030ec29fd66cf
Chris Koorzen <fckoorzen@gmail.com>
2024-11-28 23:38:15 +0200

    Add favorites page route to App

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { LandingPage } from "./page/hero";
 import { FullShowPage } from "./page/shows";
 import { BrowsePage } from "./page/browse";
 import { GenresPage } from "./page/genres";
+import { FavoritesPage } from "./page/favorites";
 
 import { loadInitialData } from "./api/server";
 
@@ -15,9 +16,8 @@ export const App = createBrowserRouter(
             <Route index element={<LandingPage />} />
             <Route path="browse/" element={<BrowsePage />} />
             <Route path="browse/genres/" element={<GenresPage />} />
-            <Route path="show/:showID/season/:seasonID" element={<FullShowPage />}>
-
-            </Route>
+            <Route path="show/:showID/season/:seasonID" element={<FullShowPage />} />
+            <Route path=":user/favorites" element={<FavoritesPage />} />
         </Route>
     )
 );
