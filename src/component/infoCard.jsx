ce7a79ad9ac310555358bff373c40705b979d8df
Chris Koorzen <fckoorzen@gmail.com>
2024-11-28 11:54:47 +0200

    Add method to make an episode 'Favorite'

--- src/component/infoCard.jsx
+++ src/component/infoCard.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useContext } from "react";
+import { Link, useOutletContext } from "react-router-dom";
+
+import { BsStar, BsStarFill } from "react-icons/bs";
 
 import { AsyncImage, ImageSlider } from "./basic";
 
 import { sanitizeHtmlLiterals } from "../utils/strings";
 import { showGenres } from "../api/server";
+import { ShowContext } from "../page/shows";
+import { users, showUUID } from "../api/storage";
 
 
 export function FullShowPreview({ show }){
@@ -34,7 +38,7 @@ export function FullShowPreview({ show }){
                 <p className="line-clamp-6">{show.description}</p>
                 <div className="mt-5 flex justify-evenly">
                     <Link
-                        to={`/show/${show.id}`}
+                        to={`/show/${show.id}/season/1`}
                         className="rounded-full p-3 bg-gray-200 text-black"
                     >
                         Read More..
@@ -47,7 +51,7 @@ export function FullShowPreview({ show }){
 };
 
 
-export function FullShowHeader({ show }){
+export function FullShowHeader({show}){
 
     return (
         <div className="text-white flex flex-col gap-5">
@@ -82,10 +86,9 @@ export function FullShowHeader({ show }){
 };
 
 
-export function SeasonSelector({ show }){
-    const [activeSeason, setActiveSeason] = useState(show.seasons[0]);
+export function SeasonSelector({ seasonID }){
+    const { show }= useContext(ShowContext);
     const sortedSeasons = show.seasons.sort((a, b)=> a.season > b.season);
-    console.log("Season Selector")
 
     function selectSeason(pos){
         setActiveSeason(sortedSeasons[pos]);
@@ -94,29 +97,94 @@ export function SeasonSelector({ show }){
     return (
         <div>
             <h1>Seasons</h1>
-            <ImageSlider setActiveIndexCallback={selectSeason}>
-                {sortedSeasons.map(el => (
-                    <AsyncImage imgUrl={el.image} className="size-40 rounded-lg"/>
+            <ImageSlider >
+                {sortedSeasons.map((el, index) => (
+                    <Link
+                        key={index}
+                        to={`/show/${show.id}/season/${el.season}`}
+                    >
+                        <AsyncImage imgUrl={el.image} className="size-40 rounded-lg"/>
+                    </Link>
                 ))}
             </ImageSlider>
-            <EpisodeView episodes={activeSeason.episodes}/>
+
+            <ShowContext.Provider value={{ show, seasonID }}>
+                <EpisodeView episodes={
+                    (show.seasons.find(el => el.season === parseInt(seasonID))).episodes
+                } />
+            </ShowContext.Provider>
         </div>
     );
 };
 
 
 function EpisodeView({ episodes }){
+    const { show, seasonID } = useContext(ShowContext);
 
     return (
         <div className="w-full flex flex-row flex-wrap">
             {
                 episodes.map((ep, index) => (
-                    <div key={index} className="bg-gray-100 p-2 my-2 rounded">
-                        <p>Episode {ep.episode}</p>
-                        <p>{ep.description}</p>
-                    </div>
+                    <Episode key={`${seasonID} ${index}`} ep={ep} />
                 ))
             }
+        </div>
+    );
+};
+
+
+function Episode({ ep }){
+    const { show, seasonID } = useContext(ShowContext);
+    const { setActiveEpisode, userID } = useOutletContext();
+
+    let userData = users.getUserData(userID);
+    let fav = false;
+    if (userData !== null){
+        fav = Object.keys(userData.favorites).includes(showUUID.get(show.id, seasonID, ep.episode));
+    };
+
+    const [isFavorite, setIsFavorite] = useState(fav);
+
+    function toggleFavorite(){
+        const userData = users.getUserData(userID);
+        console.log("toggle favs", userData)
+        if (userData === null) return;
+
+        if (isFavorite){
+            delete userData.favorites[showUUID.get(show.id, seasonID, ep.episode)];
+            users.updateData(userID, userData);
+            setIsFavorite(false);
+        } else {
+            userData.favorites[showUUID.get(show.id, seasonID, ep.episode)] = {
+                date: Date.now(),
+                showTitle: show.title,
+                episodeTitle: ep.title,
+                imgUrl: show.image
+            };  // save basic data, to avoid retrieving entire show object later
+
+            users.updateData(userID, userData);
+            setIsFavorite(true);
+        };
+    };
+
+    return (
+        <div className="bg-gray-100 p-2 my-2 rounded">
+            <p>Episode {ep.episode}</p>
+            <p>{ep.title}</p>
+            {isFavorite ?
+                <BsStarFill className="fill-green-500" onClick={toggleFavorite} /> :
+                <BsStar className="star " onClick={toggleFavorite} />
+            }
+            <p>{ep.description}</p>
+            <button
+                onClick={()=>{
+                    setActiveEpisode(
+                        show.id,
+                        seasonID,
+                        ep.episode
+                    );
+                }}
+            >Play</button>
         </div>
     );
 };
