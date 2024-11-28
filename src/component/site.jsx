faf984149f2a237ded51a7eb11c3d991cc57f21d
Chris Koorzen <fckoorzen@gmail.com>
2024-11-28 11:55:59 +0200

    Expand site footer to include meta details of playing episode

--- src/component/site.jsx
+++ src/component/site.jsx
@@ -1,14 +1,24 @@
-import { Link } from "react-router-dom";
+import { Suspense, useContext } from "react";
+import { Link, useRouteLoaderData, Await } from "react-router-dom";
 
 import {
     Navbar,
     Button,
-    Typography
+    Typography,
+    progress
 } from "@material-tailwind/react";
+
+import { AudioPlayer } from "../component/audioplayer";
 
 import iconURL from "../asset/music.png";
 
-export function SiteHeader(){
+import { users, showUUID } from "../api/storage";
+import { AudioContext } from "../page/layouts";
+
+import placeholderSound from "../dev/pitch.mp3"
+
+
+export function SiteHeader({userLogOutFn}){
 
     return (
         <Navbar
@@ -33,9 +43,68 @@ export function SiteHeader(){
 
                 <div className="flex gap-2">
                     <Button>Sign Up</Button>
-                    <Button>Log In</Button>
+                    <Button onClick={userLogOutFn}>Log Out</Button>
                 </div>
             </div>
     </Navbar>
+    );
+};
+
+
+export function SiteFooter(){
+    const { userID, episode } = useContext(AudioContext);
+    
+
+    if (episode === null){
+        return (
+            <div className="h-16 w-full bg-lime-200">
+
+            </div>
+        );
+    };
+    const { showID, seasonID, episodeID } = episode;
+    const { shows } = useRouteLoaderData("root");
+    const userData = users.getUserData(userID);
+    
+    console.log("reload footer")
+
+    return (
+        <Suspense>
+            <Await 
+                resolve={shows[showID]} 
+                children={show => {
+                    console.log("awaited", seasonID, episodeID)
+                    const season = show.seasons.find(el => el.season === parseInt(seasonID));
+                    const episode = season.episodes.find(el => el.episode === parseInt(episodeID));
+                    const url = episode.file;
+
+                    let pro = 0;
+                    if (userData !== null){
+                        pro = userData.progress[showUUID.get(showID, seasonID, episodeID)];
+                        pro = !pro ? 0 : parseInt(pro); // if undefined or null, make zero, else get progress
+                    };
+
+                    // let isFav = Object.keys(userData.favorites).includes(showUUID.get(showID, seasonID, episodeID));
+
+                    return (
+                        <div className="h-16 w-full bg-gray-900 flex flex-row">
+                            <Link to={`/show/${showID}/season/${seasonID}`} className="block w-1/2 h-full">
+                                <div className="size-full px-4 flex flex-col justify-center">
+                                    <div className="text-white flex flex-row justify-between">
+                                        <p className="font-bold">{show.title}</p>
+                                        <p>Season {seasonID}</p>
+                                    </div>
+                                    <div className="text-white flex flex-row">
+                                        <p className="pr-10 text-nowrap">Episode {episodeID}</p>
+                                        <p className="grow line-clamp-1 overflow-x-hidden text-nowrap">{episode.title}</p>
+                                    </div>
+                                </div>
+                            </Link>
+                            <AudioPlayer audio={{URL: url, progress: pro}} />
+                        </div>
+                    )
+                }}>
+            </Await>
+        </Suspense>
     );
 };
