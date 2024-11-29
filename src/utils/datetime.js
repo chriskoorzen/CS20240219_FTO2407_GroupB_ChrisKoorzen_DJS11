2476981db6b2b7eb737adfccc55b54b14247571c
Chris Koorzen <fckoorzen@gmail.com>
2024-11-29 09:46:04 +0200

    Add datetime transform utilities

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -12,3 +12,22 @@ export function secondsToClockDisplay(seconds){
     };
     return `${minutes}:${seconds > 9 ? seconds: `0${seconds}`}`;
 };
+
+
+export function timestampToMonth(timestamp){
+    return new Date(timestamp).toLocaleDateString(undefined, {year: "numeric",month: "short"})
+}
+
+
+export function timestampToDateTime(timestamp){
+    return new Date(parseInt(timestamp)).toLocaleString(
+        undefined,
+        {
+            year: "numeric",
+            month: "short",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit"
+        }
+    );
+};
